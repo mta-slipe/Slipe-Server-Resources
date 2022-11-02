1687c2a68590a8617e41bd88cca08f5958ed9896
sebajura1234@gmail.com <sebajura1234@gmail.com>
2022-11-02 20:30:07 +0100

    add noclip

---
+++ SlipeServer.Resource.NoClip/Lua/NoClip.lua
@@ -0,0 +1,92 @@
+﻿local config = {
+    verticalSpeed = 0.1,
+    horizontalSpeed = 0.2
+}
+
+local function putPlayerInPosition(timeslice)
+    if isChatBoxInputActive() or isConsoleActive() or isMainMenuActive() or isTransferBoxActive() then
+        return
+    end
+
+    local cx, cy, cz, ctx, cty, ctz = getCameraMatrix()
+    ctx, cty = ctx - cx, cty - cy
+    if(getKeyState("lshift"))then
+        timeslice = timeslice * 3
+    end
+    if(getKeyState("lctrl"))then
+        timeslice = timeslice / 3
+    end
+    if getKeyState("space") then
+        abz = abz + (timeslice * config.verticalSpeed)
+    end
+    if getKeyState("lalt")  then
+        abz = abz - (timeslice * config.verticalSpeed)
+    end
+    local mult = timeslice / math.sqrt(ctx * ctx + cty * cty) * config.horizontalSpeed
+    ctx, cty = ctx * mult, cty * mult
+    if getKeyState("2") then
+        abx, aby = abx + ctx, aby + cty
+    end
+    if getKeyState("w") then
+        abx, aby = abx + ctx, aby + cty
+    end
+    if getKeyState("s") then
+        abx, aby = abx - ctx, aby - cty
+    end
+    if getKeyState("a") then
+        abx, aby = abx - cty, aby + ctx
+    end
+    if getKeyState("d") then
+        abx, aby = abx + cty, aby - ctx
+    end
+
+    if isPedInVehicle(getLocalPlayer()) then
+        local vehicle = getPedOccupiedVehicle(getLocalPlayer())
+        local angle = getPedCameraRotation(getLocalPlayer())
+        setElementPosition(vehicle, abx, aby, abz)
+        setElementRotation(vehicle, 0, 0, -angle)
+    else
+        local angle = getPedCameraRotation(getLocalPlayer())
+        setElementRotation(getLocalPlayer(), 0, 0, angle)
+        setElementPosition(getLocalPlayer(), abx, aby, abz)
+    end
+end
+
+function setNoClipEnabled(enabled)
+    if enabled then
+        if isPedInVehicle(getLocalPlayer()) then
+            local vehicle = getPedOccupiedVehicle(getLocalPlayer())
+            abx, aby, abz = getElementPosition(vehicle)
+            setElementCollisionsEnabled(vehicle, false)
+            setElementFrozen(vehicle, true)
+            setElementAlpha(getLocalPlayer(), 0)
+            addEventHandler("onClientPreRender", root, putPlayerInPosition)
+        else
+            abx, aby, abz = getElementPosition(localPlayer)
+            setElementCollisionsEnabled(localPlayer, false)
+            addEventHandler("onClientPreRender", root, putPlayerInPosition)
+        end
+    else
+        if isPedInVehicle(getLocalPlayer()) then
+            local vehicle = getPedOccupiedVehicle(getLocalPlayer())
+            abx, aby, abz = nil
+            setElementFrozen(vehicle, false)
+            setElementCollisionsEnabled(vehicle, true)
+            setElementAlpha(getLocalPlayer(), 255)
+            removeEventHandler("onClientPreRender", root, putPlayerInPosition)
+        else
+            abx, aby, abz = nil
+            setElementCollisionsEnabled(localPlayer, true)
+            removeEventHandler("onClientPreRender", root, putPlayerInPosition)
+        end
+    end
+end
+
+addEvent("internalSetNoClipEnabled", true)
+addEventHandler("internalSetNoClipEnabled", localPlayer, setNoClipEnabled)
+
+addEvent("internalUpdateConfiguration", true)
+addEventHandler("internalUpdateConfiguration", localPlayer, function(upDown, speed)
+    config.upDown = upDown
+    config.speed = speed
+end)
---
+++ SlipeServer.Resource.NoClip/NoClipLogic.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Logging;
+using SlipeServer.Server;
+using SlipeServer.Server.Elements;
+using SlipeServer.Server.Events;
+using SlipeServer.Server.ElementCollections;
+using SlipeServer.Server.Services;
+using System.Linq;
+using SlipeServer.Server.Resources;
+using Microsoft.Extensions.Options;
+
+namespace SlipeServer.Resource.NoClip;
+
+internal class NoClipLogic
+{
+    private readonly MtaServer _server;
+    private readonly NoClipService _noClipService;
+    private readonly NoClipResource _resource;
+
+    private readonly HashSet<Player> _noClipPlayers = new();
+
+    public NoClipLogic(MtaServer server, NoClipService noClipService)
+    {
+        _server = server;
+        _noClipService = noClipService;
+        server.PlayerJoined += HandlePlayerJoin;
+
+        _resource = _server.GetAdditionalResource<NoClipResource>();
+        _noClipService.NoClipStateChanged += SetNoClipEnabled;
+    }
+
+    private void SetNoClipEnabled(Player player, bool enabled)
+    {
+        if (enabled)
+        {
+            if (_noClipPlayers.Contains(player))
+                return;
+            _noClipPlayers.Add(player);
+        }
+        else
+        {
+            if (!_noClipPlayers.Contains(player))
+                return;
+            _noClipPlayers.Remove(player);
+        }
+
+        if(_noClipPlayers.Contains(player) == enabled)
+            player.TriggerLuaEvent("internalSetNoClipEnabled", player, enabled);
+    }
+
+    private void HandlePlayerJoin(Player player)
+    {
+        _resource.StartFor(player);
+        var options = _resource.Options;
+        player.ResourceStarted += (player, @event) =>
+        {
+            if(@event.NetId == _resource.NetId)
+                player.TriggerLuaEvent("internalUpdateConfiguration", player, options.VerticalSpeed, options.HorizontalSpeed);
+        };
+        if(options.Bind != null)
+        {
+            player.SetBind(options.Bind, Server.Elements.Enums.KeyState.Up);
+            player.BindExecuted += Player_BindExecuted;
+        }
+    }
+
+    private void Player_BindExecuted(Player player, Server.Elements.Events.PlayerBindExecutedEventArgs e)
+    {
+        if(e.Key == _resource.Options.Bind && e.KeyState == Server.Elements.Enums.KeyState.Up)
+        {
+            bool isNoClipEnabled = _noClipPlayers.Contains(player);
+            if (!isNoClipEnabled)
+            {
+                if(_resource.Options.AuthorizationCallback != null)
+                {
+                    if (!_resource.Options.AuthorizationCallback(player))
+                        return;
+                }
+                SetNoClipEnabled(player, true);
+            }
+            else
+            {
+                SetNoClipEnabled(player, false);
+            }
+        }
+    }
+}
