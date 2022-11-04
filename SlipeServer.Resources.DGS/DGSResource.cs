7db906ec89f70e9679cef2d0a40b21805ffb2f29
sebajura1234@gmail.com <sebajura1234@gmail.com>
2022-11-04 17:33:58 +0100

    add dgs

---
+++ SlipeServer.Resources.DGS/DGSResource.cs
@@ -0,0 +1,86 @@
+﻿using SlipeServer.Packets.Definitions.Lua;
+using SlipeServer.Server;
+using SlipeServer.Server.Elements;
+using SlipeServer.Server.Elements.Enums;
+using SlipeServer.Server.Resources;
+using SlipeServer.Server.Resources.Interpreters.Meta;
+using System.IO.Compression;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SlipeServer.Resources.DGS;
+
+internal class DGSResource : Resource
+{
+    internal Dictionary<string, byte[]> AdditionalFiles { get; } = new();
+    private readonly Dictionary<LuaValue, LuaValue> DGSRecordedFiles = new();
+
+    internal DGSResource(MtaServer server, DGSVersion version)
+        : base(server, server.GetRequiredService<RootElement>(), "dgs")
+    {
+        var versionString = version switch
+        {
+            DGSVersion.Release_3_518 => "3.518",
+            _ => throw new NotImplementedException()
+        };
+        var downloadPath = $"https://github.com/thisdp/dgs/archive/refs/tags/{versionString}.zip";
+
+        var client = new HttpClient();
+        var response = client.GetAsync(downloadPath).Result;
+        response.EnsureSuccessStatusCode();
+
+        using var zip = new ZipArchive(response.Content.ReadAsStream(), ZipArchiveMode.Read);
+        var metaXmlEntry = zip.GetEntry($"dgs-{versionString}/meta.xml");
+
+        StreamReader streamReader = new StreamReader(metaXmlEntry.Open()); 
+        MetaXml? metaXml = (MetaXml?)new XmlSerializer(typeof(MetaXml)).Deserialize(streamReader);
+
+        using SHA256 sha256Hash = SHA256.Create();
+        foreach (MetaXmlFile item in metaXml.Value.files)
+        {
+            var entry = zip.GetEntry($"dgs-{versionString}/{item.Source}");
+            using StreamReader sr = new StreamReader(entry.Open());
+            var data = Encoding.Default.GetBytes(sr.ReadToEnd());
+            Files.Add(ResourceFileFactory.FromBytes(data, item.Source, ResourceFileType.ClientFile));
+            AdditionalFiles.Add(item.Source, data);
+            string hash = GetHash(sha256Hash, data);
+            DGSRecordedFiles.Add(item.Source, new LuaValue(new LuaValue[] {hash, data.Length }));
+        }
+
+        foreach (MetaXmlScript item in metaXml.Value.scripts.Where((MetaXmlScript x) => x.Type == "client"))
+        {
+            var entry = zip.GetEntry($"dgs-{versionString}/{item.Source}");
+            using StreamReader sr = new StreamReader(entry.Open());
+            var data = Encoding.Default.GetBytes(sr.ReadToEnd());
+            Files.Add(ResourceFileFactory.FromBytes(data, item.Source, ResourceFileType.ClientScript));
+            AdditionalFiles.Add(item.Source, data);
+        }
+
+        Exports = metaXml.Value.exports
+            .Where(x => x.Type == "client")
+            .Select(x => x.Function)
+            .ToList();
+
+        //Root.SetData("DGSI_FileInfo", DGSRecordedFiles, DataSyncType.Broadcast);
+        server.PlayerJoined += Server_PlayerJoined;
+    }
+
+    private void Server_PlayerJoined(Player obj)
+    {
+        Root.SetData("DGSI_FileInfo", DGSRecordedFiles, DataSyncType.Broadcast);
+    }
+
+    private static string GetHash(HashAlgorithm hashAlgorithm, byte[] input)
+    {
+        byte[] data = hashAlgorithm.ComputeHash(input);
+
+        var sBuilder = new StringBuilder();
+        for (int i = 0; i < data.Length; i++)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+
+        return sBuilder.ToString();
+    }
+}
---
+++ SlipeServer.Resources.DGS/ServerBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using SlipeServer.Server.ServerBuilders;
+
+namespace SlipeServer.Resources.DGS;
+
+public static class ServerBuilderExtensions
+{
+    public static void AddDGSResource(this ServerBuilder builder, DGSVersion version)
+    {
+        builder.AddBuildStep(server =>
+        {
+            var resource = new DGSResource(server, version);
+            server.AddAdditionalResource(resource, resource.AdditionalFiles);
+        });
+
+        builder.AddLogic<DGSLogic>();
+    }
+}
