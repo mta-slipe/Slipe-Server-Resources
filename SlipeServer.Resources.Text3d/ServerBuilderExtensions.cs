2706bee567be51f7b011ff469cfd53a155de8350
sebajura1234@gmail.com <sebajura1234@gmail.com>
2022-11-05 16:38:19 +0100

    add text3d resource

---
+++ SlipeServer.Resources.Text3d/ServerBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SlipeServer.Server.ServerBuilders;
+
+namespace SlipeServer.Resources.Text3d;
+
+public static class ServerBuilderExtensions
+{
+    public static void AddText3dResource(this ServerBuilder builder)
+    {
+        builder.AddBuildStep(server =>
+        {
+            var resource = new Text3dResource(server);
+            server.AddAdditionalResource(resource, resource.AdditionalFiles);
+        });
+
+        builder.ConfigureServices(services =>
+        {
+            services.AddSingleton<Text3dService>();
+        });
+
+        builder.AddLogic<Text3dLogic>();
+    }
+}
