d3b3ff4667dd0976367e91aa486f6cc7acdcb79b
Bob <bobvanhooff@outlook.com>
2022-11-12 22:45:46 +0100

    Minor cleanup

--- SlipeServer.Resources.Reload/ReloadLogic.cs
+++ SlipeServer.Resources.Reload/ReloadLogic.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
-using SlipeServer.Server;
+﻿using SlipeServer.Server;
 using SlipeServer.Server.Elements;
 using SlipeServer.Server.Events;
-using SlipeServer.Server.ElementCollections;
 using SlipeServer.Server.Services;
 
 namespace SlipeServer.Resources.Reload;
@@ -10,20 +8,11 @@ namespace SlipeServer.Resources.Reload;
 public class ReloadLogic
 {
     private readonly MtaServer server;
-    private readonly LuaEventService luaEventService;
-    private readonly ILogger logger;
-    private readonly IElementCollection elementCollection;
     private readonly ReloadResource resource;
 
-    public ReloadLogic(MtaServer server,
-        LuaEventService luaEventService,
-        ILogger logger,
-        IElementCollection elementCollection)
+    public ReloadLogic(MtaServer server, LuaEventService luaEventService)
     {
         this.server = server;
-        this.luaEventService = luaEventService;
-        this.logger = logger;
-        this.elementCollection = elementCollection;
         server.PlayerJoined += HandlePlayerJoin;
 
         luaEventService.AddEventHandler("relWep", HandleWeaponReloadRequest);
